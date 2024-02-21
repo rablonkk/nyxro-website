7a124fd6d425b04c776195c60bd6ab8fdf4e5339
pablo rafael <157351009+shynozx@users.noreply.github.com>
2024-02-21 12:47:49 -0300

    chore: small fix update

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import '../styles/globals.css';
 export default function homePage() {
   return (
     <>
-      <main className="flex flex-col justify-center items-center mt-44">
+      <main className="flex flex-col justify-center items-center mt-28">
         <div className="flex justify-center flex-col items-center">
           <span className="font-black text-7xl max-md:text-4xl text-center">
               O melhor bot de música é o
@@ -15,7 +15,7 @@ export default function homePage() {
               'WebkitTextFillColor': 'transparent',
             }} className="font-black text-9xl max-md:text-6xl">Nyxro!</h1>
           </span>
-          <p className="text-center text-xl max-md:text-sm">Nyxro é um bot de música criado e desenvolvido para tocar suas músicas favoritas!</p>
+          <p className="text-center mt-6 text-xl max-md:text-1xl">Nyxro é um bot de música criado e desenvolvido para tocar suas músicas favoritas!</p>
 
           <button className="mt-10 pt-2 pb-2 pr-10 pl-10 bg-[#FB029E] rounded-xl text-white font-semibold">
             <a href="https://discord.com/oauth2/authorize?client_id=1152354301914648636&scope=bot%20applications.commands">Adicionar ao seu servidor</a>
@@ -24,20 +24,20 @@ export default function homePage() {
       </main>
 
       {(
-        <ul className="mt-28 mb-10 w-full flex flex-wrap items-center justify-center gap-8">
-          <li className="bg-[#F9F9F9] rounded-2xl h-32 w-96 px-3 py-5 shadow-lg">
-            <h1 className="font-bold text-2xl mb-2 pl-5">Qualidade de Áudio</h1>
-            <p className="text-xl pl-5">Nyxro entrega música em alta qualidade!</p>
+        <ul className="mt-24 mb-10 w-full flex flex-wrap items-center justify-center gap-8">
+          <li className="bg-[#F9F9F9] rounded-2xl h-50 w-96 px-3 py-5 shadow-lg">
+            <h1 className="font-bold text-xl max-md:text-1xl mb-2 pl-5">Facilidade de Uso</h1>
+            <p className="text-xl max-md:text-1xl pl-5">O Nyxro oferece uma experiência simplificada para reprodução de música no Discord, com a sua interface intuitiva.</p>
           </li>
 
-          <li className="bg-[#F9F9F9] rounded-2xl h-32 w-96 px-3 py-5 shadow-lg">
-            <h1 className="font-bold text-2xl mb-2 pl-5">Biblioteca Abrangente</h1>
-            <p className="text-xl pl-5">Nyxro têm milhões de faixas de diversos gêneros musicais.</p>
+          <li className="bg-[#F9F9F9] rounded-2xl h-50 w-96 px-3 py-5 shadow-lg">
+            <h1 className="font-bold text-xl max-md:text-1xl mb-2 pl-5">Suporte Multifuncional</h1>
+            <p className="text-xl max-md:text-1xl pl-5">Além do suporte ao YouTube e SoundCloud, o Nyxro têm outras funcionalidades, como pesquisa de músicas.</p>
           </li>
 
-          <li className="bg-[#F9F9F9] rounded-2xl h-32 w-96 px-3 py-5 shadow-lg">
-            <h1 className="font-bold text-2xl mb-2 pl-5">Comandos Intuitivos</h1>
-            <p className="text-xl pl-5">Nyxro têm uma interface de usuário simplificada e intuitiva.</p>
+          <li className="bg-[#F9F9F9] rounded-2xl h-50 w-96 px-3 py-5 shadow-lg">
+            <h1 className="font-bold text-xl max-md:text-1xl mb-2 pl-5">Confiabilidade e Estabilidade</h1>
+            <p className="text-xl max-md:text-1xl pl-5">Sua infraestrutura robusta permitem que os usuários desfrutem de uma experiência sem interrupções.</p>
           </li>
         </ul>
       )}
