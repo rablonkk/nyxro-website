2d49962cbbf79256585c926ace9297a61d0d0873
shynozx.aep <157351009+shynozx@users.noreply.github.com>
2024-03-08 03:04:32 -0300

    chore: small fix update

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function homePage() {
 
   return (
     <>
-      <main className="flex flex-col justify-center items-center mt-52">
+      <main className="flex flex-col justify-center items-center mt-32">
         <div className="flex justify-center flex-col items-center">
           <span className="font-black text-7xl max-md:text-4xl text-center">
               O Melhor Bot de Música é o
