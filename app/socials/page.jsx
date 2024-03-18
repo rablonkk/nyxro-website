2d1568b3576b4d72bd5ea45b72acb3abfa334eb3
shynozx.aep <157351009+shynozx@users.noreply.github.com>
2024-03-18 13:50:33 -0300

    chore: small updates to the developers page

--- app/socials/page.jsx
+++ app/socials/page.jsx
@@ -30,8 +30,8 @@ export default function socialPage() {
       {
         profile === 'sushi' ? (
           <div className="px-3 mt-24 mb-16 w-full flex flex-col items-center justify-center">
-            <h1 className="text-xl"><span className="font-black text-3xl">ATSHUSHI</span> is a Fullstack and Software Engineer</h1>
-            <p>Atshushi made the front-end and back-end of that site</p>
+            <h1 className="text-xl"><span className="font-black text-3xl">ATSHUSHI</span> é um Programador Fullstack e Engenheiro de Software</h1>
+            <p><span className="font-black text-1xl">ATSHUSHI</span> faz parte da equipe de Front-end do projeto. Esse site foi feito por ele!</p>
 
             <div className="mt-10 flex items-center gap-10">
               <a className="flex items-center justify-center" href="https://github.com/atshushi"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg></a>
@@ -40,8 +40,8 @@ export default function socialPage() {
           </div>
         ) : profile === 'eleven' ? (
           <div className="px-3 mt-24 mb-16 w-full flex flex-col items-center justify-center">
-            <h1 className="text-xl"><span className="font-black text-3xl">Λ</span> is a Computer Engineer</h1>
-            <p>Project developer</p>
+            <h1 className="text-xl"><span className="font-black text-3xl">Λ</span> é um Engenheiro de Computação</h1>
+            <p><span className="font-black text-1xl">Λ</span> faz parte da equipe de desenvolvedores.</p>
 
             <div className="mt-10 flex items-center gap-10">
               <a className="flex items-center justify-center" href="https://github.com/nightcor-e"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg></a>
@@ -50,8 +50,8 @@ export default function socialPage() {
           </div>
         ) : profile === 'rablonkk' ? (
           <div className="px-3 mt-24 mb-16 w-full flex flex-col items-center justify-center">
-            <h1 className="text-xl"><span className="font-black text-3xl">RABLONKK'</span> is a Back-End Programmer</h1>
-            <p>Founder and main developer of the project</p>
+            <h1 className="text-xl"><span className="font-black text-3xl">RABLONKK'</span> é um Programador Back-end</h1>
+            <p><span className="font-black text-1xl">RABLONKK'</span> é o fundador e principal desenvolvedor do projeto.</p>
 
             <div className="mt-10 flex items-center gap-10">
               <a className="flex items-center justify-center" href="https://github.com/shynozx"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg></a>
@@ -60,8 +60,8 @@ export default function socialPage() {
           </div>
         ) : (
           <div className="px-3 mt-24 mb-16 w-full flex flex-col items-center justify-center">
-            <h1 className="text-xl"><span className="font-black text-3xl">KYOMI</span> is a Front-end Programmer and Software Engineer</h1>
-            <p>Project developer</p>
+            <h1 className="text-xl"><span className="font-black text-3xl">KYOMI</span> é um Programador Fullstack e Engenheiro de Software</h1>
+            <p><span className="font-black text-1xl">KYOMI</span> faz parte da equipe de desenvolvedores.</p>
 
             <div className="mt-10 flex items-center gap-10">
               <a className="flex items-center justify-center" href="https://github.com/bitterteriyaki"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path></svg></a>
