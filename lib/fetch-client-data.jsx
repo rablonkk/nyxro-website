fbf9138e71ada913d790b1d17c61b10e679339b0
pablo rafael <157351009+rablonkk@users.noreply.github.com>
2024-08-17 00:53:12 -0300

    chore: small fix update

--- lib/fetch-client-data.jsx
+++ lib/fetch-client-data.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const fetchClientData = async() => {
-  const guildsResult = await axios.get('https://api.nyxrobot.site/guilds', { timeout: 20000 });
-  const topTracksResult = await axios.get('https://api.nyxrobot.site/top-tracks', { timeout: 20000 });
-  const usersResult = await axios.get('https://api.nyxrobot.site/users', { timeout: 20000 });
+  const guildsResult = await axios.get('https://api.nyxrobot.site/guilds', { params: { per_page: 10 } });
+  const topTracksResult = await axios.get('https://api.nyxrobot.site/top-tracks', { params: { per_page: 10 } });
+  const usersResult = await axios.get('https://api.nyxrobot.site/users', { params: { per_page: 10 } });
 
   return {
     guilds: guildsResult.data.guilds,
